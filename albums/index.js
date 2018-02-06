f00afc8aa728d14661df570b18a6b048d347d8b8
Otavio Augusto <tavioalves@gmail.com>
2018-02-06 01:14:44 -0300

    Refatoração do AlbumList de dump component para Class component

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,17 @@
 // Import library to help to create a component 
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => {
-	return <Header headerText={'Albums'} />;
+	return (
+		<View>	
+			<Header headerText={'Albums'} />
+			<AlbumList />
+		</View>
+	);
 };
 
 // Render it to the device
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+    componentWillMount(){
+        console.log("Component will mount the AlbumList...");
+    }
+    
+    render(){
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    };
+};
+
+export default AlbumList;
