37ba4002c1d9b511d16b895cea6aefcb579aee7f
Otavio Augusto <tavioalves@gmail.com>
2018-02-05 23:45:52 -0300

    Construção do componente Header no app Albums

--- albums/index.js
+++ albums/index.js
@@ -1,13 +1,11 @@
 // Import library to help to create a component 
 import React from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
-
 const App = () => {
-	return (
-		<Text>Some Text</Text>
-	);
+	<Header />
 };
 
 // Render it to the device
---
+++ albums/src/components/header.js
@@ -0,0 +1,13 @@
+// Import libaries
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+return (
+        <Text>Albums!</Text>
+    );
+};
+
+// Make the component available to other parts of the app
+export default Header;1
