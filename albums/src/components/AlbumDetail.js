6951390ca84d0db63b8c0558e28ed5dad823583d
Otavio Augusto <tavioalves@gmail.com>
2018-02-06 13:59:42 -0300

    Criação e estilização do botão no CardSection

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -23,8 +24,15 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
       
       <CardSection>
-        <Image style={styles.imageStyle} source={{ uri: image }} />
+        <Image 
+          style={styles.imageStyle} 
+          source={{ uri: image }} 
+        />
       </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>  
     </Card>
   );
 };
