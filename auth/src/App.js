45e6289255d3a513f9ca205f115c36df64296265
Otavio Augusto <tavioalves@gmail.com>
2018-02-08 13:43:49 -0300

    Instalação do firebase e configuração do mesmo no app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    
+    componentWillMount() {
+        // Initialize Firebase
+        firebase.initializeApp({
+            apiKey: "AIzaSyC5FJ8HkBIujuZaxKr4kmKSUD9i6403ZIw",
+            authDomain: "auth-6b6e9.firebaseapp.com",
+            databaseURL: "https://auth-6b6e9.firebaseio.com",
+            projectId: "auth-6b6e9",
+            storageBucket: "auth-6b6e9.appspot.com",
+            messagingSenderId: "943881845234"
+        });
+    }
+
     render () {
         return (
             <View>
