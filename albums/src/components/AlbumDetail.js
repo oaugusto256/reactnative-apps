c6bc0805e30ad43fbd8f4f5bcb3272f5bc2b2984
Otavio Augusto <tavioalves@gmail.com>
2018-02-07 23:08:58 -0300

    Correção de erro, finalização do botão para abrir páginas do album e finalização do app

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
 
   return (
     <Card>
@@ -31,7 +31,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button buttonText={'Click me!'} onPress={() => Linking.openURL(url)} />
       </CardSection>  
     </Card>
   );
--- albums/src/components/Button.js
+++ albums/src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, buttonText }) => {
   const { buttonStyle, textStyle } = styles;
   
   return (
-    <TouchableOpacity style={buttonStyle}>
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={textStyle}>
-        Click me
+        {buttonText}
       </Text>
     </TouchableOpacity>
   );
