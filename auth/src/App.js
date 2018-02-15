d3d9a62fd79f05e07bbe9b4437f5e4536c3ed8c6
Otavio Augusto <tavioalves@gmail.com>
2018-02-14 21:39:29 -0300

    Verificação inicial se o usuário já esta logado ou não

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+
+  state = { loggedIn: null };
     
   componentWillMount() {
-    // Initialize Firebase
     firebase.initializeApp({
       apiKey: "AIzaSyC5FJ8HkBIujuZaxKr4kmKSUD9i6403ZIw",
       authDomain: "auth-6b6e9.firebaseapp.com",
@@ -16,14 +17,45 @@ class App extends Component {
       storageBucket: "auth-6b6e9.appspot.com",
       messagingSenderId: "943881845234"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent () {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button buttonText={'Logout'} />
+          </ CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return(
+          <Spinner/>
+      );
+    }
+
+    
   }
 
   render () {
     return (
       <View>
-        <Header headerText="Authentication" />
-        <LoginForm />
-      </View>
+        <View style={{ marginBottom: 30 }}>
+          <Header headerText="Authentication" />
+        </View>
+        <View>
+          {this.renderContent()}
+        </View>
+      </View>  
       );
   };
 }
