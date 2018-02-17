2043c79a1ed9c3414fd24a044144c7f1104dc4c0
Otavio Augusto <tavioalves@gmail.com>
2018-02-17 13:12:52 -0300

    Correção de animação

--- techstack/src/components/ListItem.js
+++ techstack/src/components/ListItem.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, NativeModules, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
+
+const { UIManager } = NativeModules
+UIManager.setLayoutAnimationEnabledExperimental
+    && UIManager.setLayoutAnimationEnabledExperimental(true)
 
 class ListItem extends Component {
   componentWillUpdate(){
