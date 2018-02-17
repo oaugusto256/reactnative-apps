cff8bbdd4168493e17fe3f05555c8ef8433fc80e
Otavio Augusto <tavioalves@gmail.com>
2018-02-16 21:48:07 -0300

    Estilização e apresentação de lista de itens usando o ListView e ListItem

--- techstack/src/components/ListItem.js
+++ techstack/src/components/ListItem.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import CardSection from './common';
+import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
+
     return(
       <CardSection>
-        <Text>{this.props.library.title}</Text>
+        <Text style={titleStyle}>{this.props.library.title}</Text>
       </CardSection>
     );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 20,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
