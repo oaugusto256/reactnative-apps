6ef2cd1a45d82d92226010eae2143077e3bbf1a0
Otavio Augusto <tavioalves@gmail.com>
2018-02-23 14:56:40 -0300

    Criação inicial do App.js

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,3 +1,21 @@
 
 import React, { Component } from 'react';
 import { Text,  View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore()}>
+        <View>
+          <Text>
+            Manager
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
