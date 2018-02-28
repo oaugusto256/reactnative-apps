016e57632d118ac986f3be41429a3a19390a1dfe
Otavio Augusto <tavioalves@gmail.com>
2018-02-27 23:44:08 -0300

    Correção de bugs e instalação da lib do Firebase

--- manager/src/App.js
+++ manager/src/App.js
@@ -3,9 +3,23 @@ import React, { Component } from 'react';
 import { Text,  View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount(){
+    const config = {
+      apiKey: 'AIzaSyAuZvYeGpYIv7KxtPIrmcZzxlH-U08jIKQ',
+      authDomain: 'manager-e718c.firebaseapp.com',
+      databaseURL: 'https://manager-e718c.firebaseio.com',
+      projectId: 'manager-e718c',
+      storageBucket: 'manager-e718c.appspot.com',
+      messagingSenderId: '946853046668'
+    };
+
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <Provider store={createStore(reducers)}>
