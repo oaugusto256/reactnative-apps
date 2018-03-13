c445bba23b445321aca284db339106e839e9d65b
Otavio Augusto <tavioalves@gmail.com>
2018-03-12 22:23:25 -0300

    Criação de type e handling de type com action e reducer

---
+++ manager/src/reducers/AuthReducer.js
@@ -0,0 +1,10 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
+const INITIAL_STATE = { email: '' };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
