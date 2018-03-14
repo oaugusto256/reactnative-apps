acd1cfd720b5d75b521520cfcf24f221142f37ed
Otavio Augusto <tavioalves@gmail.com>
2018-03-13 23:04:38 -0300

    Funcionalidade de auth e dispatch com redux com async

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -10,6 +10,12 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -33,7 +39,10 @@ class LoginForm extends Component {
           />
         </CardSection>
         <CardSection>
-          <Button buttonText="Login"/>
+          <Button 
+            buttonText="Login"
+            onPress={this.onButtonPress.bind(this)}
+          />
         </CardSection>
       </Card>
     );
@@ -49,5 +58,6 @@ const mapStateToProps = state => {
 
 export default connect(mapStateToProps, { 
   emailChanged,
-  passwordChanged
+  passwordChanged,
+  loginUser
 })(LoginForm);
